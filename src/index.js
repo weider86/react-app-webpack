e778bb51400b24b89a776eda0f7cf5bdfee62aa5
weider86 <weider86@gmail.com>
2022-05-06 10:54:43 -0300

    add app basic structure

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { App } from './components/App.jsx';
+
+const Main = () => {
+  return <App />;
+};
+
+ReactDOM.render(<Main />, document.getElementById('root'));
